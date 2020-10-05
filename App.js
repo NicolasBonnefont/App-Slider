05057b4c083254eae483236760591c1e5d946ea2
Nicolas <nicolas.7l@hotmail.com>
2020-10-05 11:51:58 -0300

    Finalização app slider

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, } from 'react-native';
+import Slider from '@react-native-community/slider';
+
 
 export default function App() {
+
+
+  const [slide, setSlide] = useState(0)
+
+  function atualizaSlide(e) {
+
+    setSlide(e)
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.full}>
+      <View style={styles.container}>
+        <Text>Valor do Slide: {slide} </Text>
+        <Slider
+          style={{ width: 200, height: 20 }}
+          minimumValue={0}
+          maximumValue={100}
+          onValueChange={(e) => atualizaSlide(e.toFixed(0))}
+
+        />
+      </View>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //flex: 1,
+    marginTop: 15,
   },
+  full: {
+    flex: 1
+  }
 });
